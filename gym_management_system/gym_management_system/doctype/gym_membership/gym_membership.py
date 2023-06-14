7f0fed024c16a4c97db0630e2ee26b1ec5b65c59
av-dev2 <av-dev2@aakvatech.com>
2023-06-14 12:20:07 +0300

    feat: validate gym membership and set date range of membership

--- gym_management_system/gym_management_system/doctype/gym_membership/gym_membership.py
+++ gym_management_system/gym_management_system/doctype/gym_membership/gym_membership.py
@@ -10,3 +10,27 @@ class GymMembership(Document):
 		if not self.membership_option:
 			frappe.throw("Please select a membership option")
 	
+	def validate(self):
+		if not self.membership_option:
+			frappe.throw("Please select a membership option")
+		
+		self.membership_duration = frappe.get_doc("Gym Membership Option", self.membership_option).get_days()
+		self.set_date_range()
+
+	def set_date_range(self):
+		prev_membership = frappe.get_all("Gym Membership", 
+			filters={"gym_member": self.gym_member, "status": "Active", "docstatus": 1},
+			fields=["name", "start_date", "end_date"],
+			order_by="end_date desc", limit_page_length=1
+		)
+		if len(prev_membership) > 0:
+			if not self.valid_after_end_of_prev_membership:
+				frappe.throw("This member already has an active membership")
+			else:
+				self.start_date = prev_membership[0].end_date
+				self.end_date = add_days(self.start_date, self.membership_duration)
+				self.prev_membership = prev_membership[0].name
+		else:
+			self.start_date = nowdate()
+			self.end_date = add_days(self.start_date, self.membership_duration)
+		
