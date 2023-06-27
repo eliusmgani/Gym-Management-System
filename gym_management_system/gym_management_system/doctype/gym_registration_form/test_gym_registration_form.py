222ce207f651badab1cfc86784ddc8922f490517
av-dev2 <av-dev2@aakvatech.com>
2023-06-27 11:56:48 +0300

    chore: remove gym registration form

--- gym_management_system/gym_management_system/doctype/gym_registration_form/test_gym_registration_form.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, Elius Mgani and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestGymRegistrationForm(FrappeTestCase):
-	pass
