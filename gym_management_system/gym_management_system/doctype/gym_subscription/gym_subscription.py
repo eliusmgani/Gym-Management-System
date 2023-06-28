4bf98a848a49f258bc319ac0712e7ce172325727
av-dev2 <av-dev2@aakvatech.com>
2023-06-28 16:33:31 +0300

    feat: set missing values before creating gym subscription document

--- gym_management_system/gym_management_system/doctype/gym_subscription/gym_subscription.py
+++ gym_management_system/gym_management_system/doctype/gym_subscription/gym_subscription.py
@@ -6,4 +6,15 @@ from frappe.model.document import Document
 from frappe.utils import getdate, nowdate, nowtime
 
 class GymSubscription(Document):
-	pass
+	def before_insert(self):
+		self.set_missing_values()
+	
+	def set_missing_values(self):
+		self.posting_date = nowdate()
+		self.posting_time = nowtime()
+
+		if self.gym_plan:
+			self.workout_days, self.workout_duration = frappe.get_value(
+				"Gym Workout Plan", self.gym_plan, ["workout_days", "workout_duration"]
+			)
+	
