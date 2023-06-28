72831ed0b8ed05544aff89d69b6b1da4c84ea496
av-dev2 <av-dev2@aakvatech.com>
2023-06-28 16:31:42 +0300

    feat: allow gym member to to select and subscribe to gym workout plan

---
+++ gym_management_system/gym_management_system/doctype/gym_subscription/gym_subscription.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Elius Mgani and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+from frappe.utils import getdate, nowdate, nowtime
+
+class GymSubscription(Document):
+	pass
---
+++ gym_management_system/gym_management_system/doctype/gym_subscription/test_gym_subscription.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Elius Mgani and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestGymSubscription(FrappeTestCase):
+	pass
