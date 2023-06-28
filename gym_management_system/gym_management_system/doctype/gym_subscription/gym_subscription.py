07a58e1ad89465c321b8b8ec5c199508f2b59860
av-dev2 <av-dev2@aakvatech.com>
2023-06-28 16:35:10 +0300

    feat: validate gym membership for a selected gym member

--- gym_management_system/gym_management_system/doctype/gym_subscription/gym_subscription.py
+++ gym_management_system/gym_management_system/doctype/gym_subscription/gym_subscription.py
@@ -18,3 +18,16 @@ class GymSubscription(Document):
 				"Gym Workout Plan", self.gym_plan, ["workout_days", "workout_duration"]
 			)
 	
+	def validate(self):
+		self.validate_membership()
+	
+	def validate_membership(self):
+		if self.gym_member:
+			membership = frappe.get_all(
+				"Gym Membership", 
+				filters={"gym_member": self.gym_member, "status": "Active"}, fields=["name", "status"],
+				limit_page_length=1
+			)
+
+			if len(membership) == 0:
+				frappe.throw(f"Please activate membership for this member: <strong>{self.gym_member}</strong> first")
