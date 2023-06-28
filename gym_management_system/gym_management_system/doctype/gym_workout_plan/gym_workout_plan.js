ad992140aa2043b9528ba48451534f61456d0623
av-dev2 <av-dev2@aakvatech.com>
2023-06-28 11:38:55 +0300

    feat: update gym exercise when selecting gym workout

--- gym_management_system/gym_management_system/doctype/gym_workout_plan/gym_workout_plan.js
+++ gym_management_system/gym_management_system/doctype/gym_workout_plan/gym_workout_plan.js
@@ -28,3 +28,24 @@ frappe.ui.form.on('Gym Workout Plan', {
 		});
 	}
 });
+
+frappe.ui.form.on("Gym Workout Plan Detail", {
+	gym_workout: (frm, cdt, cdn) => {
+		let row = locals[cdt][cdn];
+		if (row.gym_workout) {
+			frappe.db.get_doc("Gym Workout", row.gym_workout)
+				.then(workout_doc => {
+					for (let d of workout_doc.workout_exercise) {
+						let child = frm.add_child("workout_exercise");
+						child.exercise_name = d.exercise_name;
+						child.exercise_rounds = d.exercise_rounds;
+						child.exercise_sets = d.exercise_sets;
+						child.rest_time_per_round = d.rest_time_per_round;
+						child.exercise_type = d.exercise_type;
+						child.total_time = d.total_time;
+					}
+					frm.refresh_field("workout_exercise")
+				})
+		}
+	}
+})
