ac138a496029439de9c736333ff7209066bf9e32
av-dev2 <av-dev2@aakvatech.com>
2023-06-29 17:14:19 +0300

    feat: validate membership on gym payment if reference doctype is not 'gym membership'

--- gym_management_system/gym_management_system/doctype/gym_payment/gym_payment.py
+++ gym_management_system/gym_management_system/doctype/gym_payment/gym_payment.py
@@ -10,7 +10,21 @@ class GymPayment(Document):
 		self.set_posting_date()
 		self.set_payment_date()
 
+	def validate(self):
+		self.validate_membership()
+
 	def set_posting_date(self):
 		self.posting_date = nowdate()
 		self.posting_time = nowtime()
+
+	def validate_membership(self):
+		if self.gym_member and self.reference_doctype != "Gym Membership":
+			membership = frappe.get_all(
+				"Gym Membership", 
+				filters={"gym_member": self.gym_member, "status": "Active"}, fields=["name", "status"],
+				limit_page_length=1
+			)
+
+			if len(membership) == 0:
+				frappe.throw(f"Please activate membership for this member: <strong>{self.gym_member}</strong> first")
 	
