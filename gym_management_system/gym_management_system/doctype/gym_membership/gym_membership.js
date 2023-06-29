3eacb9a9534ad0dee87a71bb8684a527ca44173c
av-dev2 <av-dev2@aakvatech.com>
2023-06-29 16:46:44 +0300

    feat: show button to make payment for gym membership

--- gym_management_system/gym_management_system/doctype/gym_membership/gym_membership.js
+++ gym_management_system/gym_management_system/doctype/gym_membership/gym_membership.js
@@ -2,7 +2,25 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Gym Membership', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: (frm) => {
+		if (frm.doc.paid == 0 && !frm.doc.__islocal) {
+			frm.add_custom_button(__("Make Paymnent"), () => {
+				frappe.new_doc("Gym Payment", {
+					"payment_for": "Membership"
+				}, doc => {
+					doc.gym_member = frm.doc.gym_member;
+					doc.reference_doctype = frm.doc.doctype;
+					doc.reference_name = frm.doc.name;
+					doc.payment_date = frappe.datetime.now_date();
+					doc.payment_time = frappe.datetime.now_time();
+					doc.posting_date = frappe.datetime.now_date();
+					doc.posting_time = frappe.datetime.now_time();
+					doc.actual_price = frm.doc.membership_cost;
+					doc.discount_amount = frm.doc.discount_amount;
+					doc.paid_amount = frm.doc.membership_cost - frm.doc.discount_amount;
+				});
+			}).removeClass("btn-default").addClass("btn-warning font-weight-bold")
+		}
+	}
 });
+ 
