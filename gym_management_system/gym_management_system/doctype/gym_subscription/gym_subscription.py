d7b1d44e469ea616b53c96dca65e638e1484438a
av-dev2 <av-dev2@aakvatech.com>
2023-06-29 16:52:46 +0300

    feat: set subscription start and end dates

--- gym_management_system/gym_management_system/doctype/gym_subscription/gym_subscription.py
+++ gym_management_system/gym_management_system/doctype/gym_subscription/gym_subscription.py
@@ -3,7 +3,7 @@
 
 import frappe
 from frappe.model.document import Document
-from frappe.utils import getdate, nowdate, nowtime
+from frappe.utils import getdate, nowdate, nowtime, add_days
 
 class GymSubscription(Document):
 	def before_insert(self):
@@ -11,10 +11,22 @@ class GymSubscription(Document):
 		
 	def validate(self):
 		self.validate_membership()
+		self.set_subscription_dates()
 		self.set_subscription_status()
 	
 	def before_submit(self):
 		self.validate_payments()
+	
+	def before_cancel(self):
+		gym_payment = frappe.get_value("Gym Payment", 
+				{"reference_doctype": self.doctype, "reference_name": self.name, "docstatus": ["<", 2]})
+		if gym_payment:
+			pay_doc = frappe.get_doc("Gym Payment", gym_payment)
+			if pay_doc.docstatus == 0:
+				pay_doc.delete()
+			pay_doc.flags.ignore_links = True
+			pay_doc.cancel()
+			pay_doc.delete()
 	
 	def set_missing_values(self):
 		self.posting_date = nowdate()
@@ -36,12 +48,21 @@ class GymSubscription(Document):
 			if len(membership) == 0:
 				frappe.throw(f"Please activate membership for this member: <strong>{self.gym_member}</strong> first")
 	
+	def set_subscription_dates(self):
+		if self.workout_days:
+			if self.payment_date:
+				self.start_date = self.payment_date
+			else:
+				self.start_date = self.posting_date
+			self.end_date = add_days(self.start_date, self.workout_days)
+
+
 	def set_subscription_status(self, caller=None):
 		status = ""
 		if (
 			self.paid == 1 and 
 			(
-				self.start_date and getdate(nowdate()) > getdate(self.start_date)
+				self.start_date and getdate(nowdate()) >= getdate(self.start_date)
 				and self.end_date and getdate(nowdate()) <= getdate(self.end_date)
 			)
 		):
@@ -58,14 +79,13 @@ class GymSubscription(Document):
 			status = "Pending"
 		
 		if not caller:
-			self.status = status
+			self.subscription_status = status
 		else:
-			frappe.db.set_value("Gym Subscription", self.name, "status", status)
-	
+			frappe.db.set_value("Gym Subscription", self.name, "subscription_status", status)
 	
 	def validate_payments(self):
 		if self.paid == 0:
-			frappe.throw("Please pay the subscription first")
+			frappe.throw("Please pay for this subscription first")
 		
 		if self.paid_amount == 0:
 			frappe.throw("Paid amount cannot be zero")
