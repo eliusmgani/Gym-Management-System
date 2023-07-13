a66a2dd8c0bbfdb629f57fa65e892952f8470832
eliusmgani <eliasmgani@gmail.com>
2023-07-13 17:51:59 +0300

    feat: button to make payment for gym locker

--- gym_management_system/gym_management_system/doctype/gym_locker_booking/gym_locker_booking.js
+++ gym_management_system/gym_management_system/doctype/gym_locker_booking/gym_locker_booking.js
@@ -11,6 +11,23 @@ frappe.ui.form.on('Gym Locker Booking', {
 			}
 		});
 		
-
+		if (frm.doc.paid == 0 && !frm.doc.__islocal) {
+			frm.add_custom_button(__("Make Paymnent"), () => {
+				frappe.new_doc("Gym Payment", {
+					"payment_for": "Locker Booking"
+				}, doc => {
+					doc.gym_member = frm.doc.gym_member;
+					doc.reference_doctype = frm.doc.doctype;
+					doc.reference_name = frm.doc.name;
+					doc.payment_date = frappe.datetime.now_date();
+					doc.payment_time = frappe.datetime.now_time();
+					doc.posting_date = frappe.datetime.now_date();
+					doc.posting_time = frappe.datetime.now_time();
+					doc.actual_price = frm.doc.actual_price;
+					doc.discount_amount = frm.doc.discount_amount;
+					doc.paid_amount = frm.doc.actual_price - frm.doc.discount_amount;
+				});
+			}).removeClass("btn-default").addClass("btn-warning font-weight-bold")
+		}
 	}
 });
