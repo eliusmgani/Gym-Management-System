f2b65da8674a35ffc77bba7a74b463ed2f7a2c84
eliusmgani <eliasmgani@gmail.com>
2023-07-13 17:56:38 +0300

    feat: validate membership, only active gym member can request for a locker

--- gym_management_system/gym_management_system/doctype/gym_locker_booking/gym_locker_booking.py
+++ gym_management_system/gym_management_system/doctype/gym_locker_booking/gym_locker_booking.py
@@ -9,7 +9,20 @@ class GymLockerBooking(Document):
 	def before_insert(self):
 		self.set_missing_values()
 	
+	def validate(self):
+		self.validate_membership()
+
 	def set_missing_values(self):
 		self.posting_date = nowdate()
 		self.posting_time = nowtime()
 		self.issued_date = nowdate()
+	
+	def validate_membership(self):
+		if self.gym_member:
+			membership = frappe.get_all(
+				"Gym Membership",
+				filters = { "gym_member": self.gym_member, "status": "Active" }, fields = ["name", "status"],
+				limit_page_length = 1
+			)
+			if len(membership) == 0:
+				frappe.throw(f"Please activate membership for this member: <strong>{self.gym_member}</strong> first")
