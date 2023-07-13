b6b69470c3d1811f0fb1fd591f0c77b2d870283e
eliusmgani <eliasmgani@gmail.com>
2023-07-13 17:37:44 +0300

    feat: gym locker booking for requesting gym locker

---
+++ gym_management_system/gym_management_system/doctype/gym_locker_booking/gym_locker_booking.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2023, Elius Mgani and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Gym Locker Booking', {
+	refresh: function (frm) {
+		
+		
+
+	}
+});
---
+++ gym_management_system/gym_management_system/doctype/gym_locker_booking/gym_locker_booking.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Elius Mgani and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.utils import nowdate, nowtime, add_days
+from frappe.model.document import Document
+
+class GymLockerBooking(Document):
+	pass
