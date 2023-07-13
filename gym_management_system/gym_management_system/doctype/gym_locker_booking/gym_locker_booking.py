5ecf848aa4d4035e6457b837762d908a9b079563
eliusmgani <eliasmgani@gmail.com>
2023-07-13 18:02:03 +0300

    feat: validate payment before submit locker booking

--- gym_management_system/gym_management_system/doctype/gym_locker_booking/gym_locker_booking.py
+++ gym_management_system/gym_management_system/doctype/gym_locker_booking/gym_locker_booking.py
@@ -23,6 +23,10 @@ class GymLockerBooking(Document):
 			pay_doc.flags.ignore_links = True
 			pay_doc.cancel()
 			pay_doc.delete()
+	
+	def before_submit(self):
+		self.validate_payments()
+		self.set_issued_and_return_date()
 
 	def set_missing_values(self):
 		self.posting_date = nowdate()
@@ -38,3 +42,16 @@ class GymLockerBooking(Document):
 			)
 			if len(membership) == 0:
 				frappe.throw(f"Please activate membership for this member: <strong>{self.gym_member}</strong> first")
+	
+	def validate_payments(self):
+		if self.paid == 0:
+			frappe.throw("Please pay for this locker first")
+
+		if self.paid_amount == 0:
+			frappe.throw("Paid amount cannot be zero")
+
+	def set_issued_and_return_date(self):
+		self.issued_date = nowdate()
+		if self.duration:
+			self.returning_date = add_days(self.issued_date, self.duration)
+				
