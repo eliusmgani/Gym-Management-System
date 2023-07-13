547fad255f7273b9a0cd62fc0c4127164c1a6f95
eliusmgani <eliasmgani@gmail.com>
2023-07-13 17:53:51 +0300

    feat: set missing values before insert of gym locker booking

--- gym_management_system/gym_management_system/doctype/gym_locker_booking/gym_locker_booking.py
+++ gym_management_system/gym_management_system/doctype/gym_locker_booking/gym_locker_booking.py
@@ -6,4 +6,10 @@ from frappe.utils import nowdate, nowtime, add_days
 from frappe.model.document import Document
 
 class GymLockerBooking(Document):
-	pass
+	def before_insert(self):
+		self.set_missing_values()
+	
+	def set_missing_values(self):
+		self.posting_date = nowdate()
+		self.posting_time = nowtime()
+		self.issued_date = nowdate()
