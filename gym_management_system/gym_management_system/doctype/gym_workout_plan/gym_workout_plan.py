6d336dad804804631e4db0947ba2da0bd25b34c3
eliusmgani <eliasmgani@gmail.com>
2023-07-13 07:20:30 +0300

    feat: set the doctype for website view

--- gym_management_system/gym_management_system/doctype/gym_workout_plan/gym_workout_plan.py
+++ gym_management_system/gym_management_system/doctype/gym_workout_plan/gym_workout_plan.py
@@ -2,9 +2,9 @@
 # For license information, please see license.txt
 
 import frappe
-from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
 
-class GymWorkoutPlan(Document):
+class GymWorkoutPlan(WebsiteGenerator):
 	def validate(self):
 		self.set_workout_duration()
 
